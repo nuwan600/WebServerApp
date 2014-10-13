d995a5540647c9a8f55b9764a95af6c3fd3d41ba
nuwan600 <nuwan600@gmail.com>
2014-10-14 02:44:01 +0530

    WebServerApp

---
+++ src/java/bs/Oparations.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bs;
+
+import db.Dbconection;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author Dumindu
+ */
+public class Oparations {
+    
+    Connection con = Dbconection.getConnection();
+    Statement statement = null;
+    ResultSet resultSet = null;
+    
+    
+   public  List<person> getdata()
+           
+   {
+       List<person> personlist = new ArrayList<person>();
+       
+       
+   try {
+            statement = con.createStatement();
+            resultSet = statement.executeQuery("select * from  EAD.PERSON");
+            
+            while (resultSet.next()) {
+                person p = new person();
+
+                p.setId(resultSet.getInt("id"));
+                p.setName(resultSet.getString("name"));
+                p.setAddress(resultSet.getString("address"));
+                
+                System.out.println("name");
+
+                personlist.add(p);
+            }
+        } catch (SQLException ex) {
+            
+        } finally {
+            
+        }
+        return personlist;
+    }
+}
